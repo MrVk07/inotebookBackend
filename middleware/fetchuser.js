e97c7492f19f45cee15b912269ff056253c40962
Vineet Karwa <vineetkarwa123@gmail.com>
2023-01-16 15:50:11 +0530

    minor change

--- middleware/fetchuser.js
+++ middleware/fetchuser.js
@@ -6,7 +6,7 @@ const fetchuser = (req, res, next) => {
     // Get user from jwt token and add id to req object
     const token = req.header('auth-token')
     if (!token) {
-        res.status(401).send({ error: "Please authenciate using a valid token" })
+        res.status(401).send({ error: "Please enter a valid token" })
     }
     try {
         const data = jwt.verify(token, JWT_SECRET)
